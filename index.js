15a8681c60275353e77ccce4cad33f1be5fb281d
ishida <webmaster@joe1cafe.com>
2025-08-27 08:28:57 +0800

    chore: update build script
    puppeteer somehow doesnt work w/o specify local browser

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+// const puppeteer = require('puppeteer');
+
+
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Cookie thief app listening at http://localhost:${port}`);
 });
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.json({ cookies: req.cookies });
 });
+
+// const getVersion = async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   // Navigate to a page to access the navigator object
+//   await page.goto('https://www.apple.com');
+
+//   const appVersion = await page.evaluate(() => navigator.appVersion);
+//   console.log('App Version:', appVersion);
+
+//   await browser.close();
+//   return appVersion;
+// };
